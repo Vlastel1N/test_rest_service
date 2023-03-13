9368eba6c88f345a1f89e7758dbf020cf8583620
andrey kugubaev <redbrewa@gmail.com>
2023-03-13 16:09:28 +0500

    new classes

--- src/main/java/ru/students/test_rest_service/model/Request.java
+++ src/main/java/ru/students/test_rest_service/model/Request.java
@@ -1,4 +1,19 @@
 package ru.students.test_rest_service.model;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Max;
+
+
+@Data
+@Builder
 public class Request {
+
+    @NotBlank
+
 }
